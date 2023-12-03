776bf99b9f5dc23297dab557f805677160cbc5fe
Shahram <ardalan.shahram@gmail.com>
2023-12-03 15:17:03 +0330

    add fetch function to Overview function

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProjectCard from "../components/ProjectCard";
+import { Octokit } from "octokit";
 
 function Overview() {
+  const [items, setItems] = useState([]);
+  const [users] = useState("shahramardalan");
+
+  useEffect(() => {
+    const fetchRepos = async () => {
+      const res = await fetch(
+        `https://api.github.com/users/${users}/repos?page=1&per_page=10&sort=updated`
+      );
+      const data = await res.json();
+      setItems(data);
+    };
+
+    fetchRepos();
+  }, []);
+
   return (
     <div className="container">
       <div className="flex justify-between mb-2 px-2">
@@ -15,6 +31,7 @@ function Overview() {
         </div>
       </div>
       <div className="grid grid-cols-2 gap-4">
+        <h1>{items.length}</h1>
         <ProjectCard />
         <ProjectCard />
         <ProjectCard />
